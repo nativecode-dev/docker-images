16c2169b7674d3115b10c76e49763c22677491f9
Mike Pham <mpham@nativecode.com>
2016-06-11 04:22:29 -0400

    fixing export

--- teamcity-agent/build.sh
+++ teamcity-agent/build.sh
@@ -6,8 +6,8 @@ fi
 
 mkdir /tmp/test-services -p;
 
-TEAMCITY_AGENT_DIR=/tmp/test-services
-TEAMCITY_AGENT_NAME=test-agent
-TEAMCITY_SERVER_URL="http://localhost"
+export TEAMCITY_AGENT_DIR=/tmp/test-services
+export TEAMCITY_AGENT_NAME=test-agent
+export TEAMCITY_SERVER_URL="http://localhost"
 
 docker-compose up;
